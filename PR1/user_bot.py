5b9a02d71410a331ebd3117176fc8385a7e956d2
Arseny Antonov <un57a6l3@gmail.com>
2022-02-12 17:58:40 +0300

    Add files via upload

---
+++ PR1/user_bot.py
@@ -0,0 +1,232 @@
+﻿import random
+import user_bot_gh
+
+
+def near_gold(check, x, y, r):
+    for dx in range(-r, r):
+        for dy in range(-r, r):
+            if check("gold", x + dx, y + dy):
+                if dx > 0 and not check("wall", x + 1, y):
+                    return "right"
+                if dx < 0 and not check("wall", x - 1, y):
+                    return "left"
+                if dy > 0 and not check("wall", x, y + 1):
+                    return "down"
+                if dy < 0 and not check("wall", x, y - 1):
+                    return "up"
+    return "pass"
+
+
+def far_wall(check, x, y):
+    left = right = up = down = 1
+    while True:
+        if check("wall", x + right, y):
+            break
+        else:
+            right += 1
+    while True:
+        if check("wall", x - left, y):
+            break
+        else:
+            left += 1
+    while True:
+        if check("wall", x, y + down):
+            break
+        else:
+            down += 1
+    while True:
+        if check("wall", x, y - up):
+            break
+        else:
+            up += 1
+    top = max(left, right, up, down)
+    if top == right:
+        return "right"
+    if top == down:
+        return "down"
+    if top == left:
+        return "left"
+    if top == up:
+        return "up"
+    return "pass"
+
+
+def line_gold(check, x, y):
+    left = right = up = down = 1
+    while True:
+        if check("gold", x + right, y):
+            break
+        if check("wall", x + right, y):
+            right = 9999
+            break
+        else:
+            right += 1
+    while True:
+        if check("gold", x - left, y):
+            break
+        if check("wall", x - left, y):
+            left = 9999
+            break
+        else:
+            left += 1
+    while True:
+        if check("gold", x, y + down):
+            break
+        if check("wall", x, y + down):
+            down = 9999
+            break
+        else:
+            down += 1
+    while True:
+        if check("gold", x, y - up):
+            break
+        if check("wall", x, y - up):
+            up = 9999
+            break
+        else:
+            up += 1
+    if left == right == up == down == 9999:
+        return "pass"
+    else:
+        floor = min(left, right, up, down)
+        if floor == right:
+            return "right"
+        if floor == down:
+            return "down"
+        if floor == left:
+            return "left"
+        if floor == up:
+            return "up"
+    return "pass"
+
+
+def move_somewhere(check, x, y):
+    if not check("wall", x + 1, y):
+        return "right"
+    if not check("wall", x, y + 1):
+        return "down"
+    if not check("wall", x - 1, y):
+        return "left"
+    if not check("wall", x, y - 1):
+        return "up"
+    return "pass"
+
+
+def move_random(check, x, y):
+    return random.choice(["left", "right", "up", "down"])
+
+
+# doesn't work for shit
+def pathfind_gold_step(check, x, y, r, dir, path):
+    if r > 10:
+        return False
+    if check("gold", x, y):
+        path.append(dir)
+        return True
+    if check("wall", x, y):
+        return False
+    if not dir == "left":
+        if pathfind_gold_step(check, x + 1, y, r + 1, "right", path):
+            path.append(dir)
+            return True
+    if not dir == "up":
+        if pathfind_gold_step(check, x, y + 1, r + 1, "down", path):
+            path.append(dir)
+            return True
+    if not dir == "right":
+        if pathfind_gold_step(check, x - 1, y, r + 1, "left", path):
+            path.append(dir)
+            return True
+    if not dir == "down":
+        if pathfind_gold_step(check, x, y - 1, r + 1, "up", path):
+            path.append(dir)
+            return True
+    return False
+
+
+# nuclear option (failed)
+def pathfind_gold(check, x, y):
+    path = list()
+    if pathfind_gold_step(check, x, y, 0, "", path):
+        last = path.pop()
+        if last == "":
+            last = path.pop()
+        return last
+    return "pass"
+
+
+class Coords:
+    def __init__(self):
+        self.x2 = -1
+        self.y2 = -1
+        self.x1 = -1
+        self.y1 = -1
+        self.ticks = 0
+        self.wall = False
+
+    def update(self, x, y):
+        self.x2 = self.x1
+        self.y2 = self.y1
+        self.x1 = x
+        self.y1 = y
+
+    def stuttercheck(self, x, y):
+        if self.x2 == -1 or self.y2 == -1 or self.x1 == -1 or self.y1 == -1:
+            return False
+        if self.x2 == x and self.y2 == y and (self.x1 != x or self.y1 != y):
+            self.ticks = 10
+            return True
+        else:
+            return False
+
+
+coords = Coords()
+
+
+def script(check, x, y):
+    # on 4th level override the algorithm with Krutix's script cause mine fails. yikes.
+    if check("level") == 4:
+        return user_bot_gh.script(check, x, y)
+    # obvious first step
+    if check("gold", x, y):
+        return "take"
+    # trying to detect stutter
+    if coords.ticks > 0:
+        coords.update(x, y)
+        coords.ticks -= 1
+        if coords.wall:
+            cmd = move_somewhere(check, x, y)
+        else:
+            cmd = far_wall(check, x, y)
+        if coords.ticks == 0:
+            coords.wall = not coords.wall
+        if cmd != "pass":
+            return cmd
+    if coords.stuttercheck(x, y):
+        return "right"
+    coords.update(x, y)
+    # checking for gold within 2 tiles
+    cmd = near_gold(check, x, y, 2)
+    if cmd != "pass":
+        return cmd
+    # checking for gold in straight line
+    cmd = line_gold(check, x, y)
+    if cmd != "pass":
+        return cmd
+    # checking for gold within 6 tiles
+    cmd = near_gold(check, x, y, 6)
+    if cmd != "pass":
+        return cmd
+    # going towards the farthest wall
+    cmd = far_wall(check, x, y)
+    if cmd != "pass":
+        return cmd
+    # if gold not found, random movement
+    if check("level") == 3:
+        cmd = move_somewhere(check, x, y)
+    else:
+        cmd = move_random(check, x, y)
+    if cmd != "pass":
+        return cmd
+    # if all else fails
+    return "pass"
---
+++ PR1/user_bot_gh.py
@@ -0,0 +1,128 @@
+﻿# This algorithm was taken from Krutix's github and altered slightly
+# Because my script fails to pass level 4 and I'm done trying to fix that
+# See, I'm an honest student, so please pass this task for me
+# full link: https://github.com/Krutix/DandyBot/blob/main/user_bot.py
+
+
+import random
+
+
+class PathFindError(Exception):
+    pass
+
+
+def random_work(): return random.choice(['left', 'right', 'right', 'up', 'down'])
+
+
+def potential_len(xy, t): return abs(xy[0] - t[0]) + abs(xy[1] - t[1])
+
+
+def gold_value(check, x, y) -> int:
+    value = check('gold', x, y)
+
+    value += check('gold', x + 1, y)
+    value += check('gold', x + 1, y - 1)
+    value += check('gold', x + 1, y + 1)
+
+    value += check('gold', x - 1, y)
+    value += check('gold', x - 1, y - 1)
+    value += check('gold', x - 1, y + 1)
+
+    value += check('gold', x - 2, y)
+    value += check('gold', x + 2, y)
+    value += check('gold', x, y + 2)
+    value += check('gold', x, y - 2)
+    return value
+
+
+def find_gold(check, x, y, depth) -> [(int, int)]:
+    gold = set()
+    for i in range(depth * 2 + 1):
+        if check('gold', x - depth + i, y - depth):
+            gold |= {(x - depth + i, y - depth)}
+        if check('gold', x - depth + i, y + depth):
+            gold |= {(x - depth + i, y + depth)}
+        if check('gold', x - depth, y - depth + i):
+            gold |= {(x - depth, y - depth + i)}
+        if check('gold', x + depth, y - depth + i):
+            gold |= {(x + depth, y - depth + i)}
+    return gold
+
+
+def check_and_append(check, a_star, a_star_back, t, min_cell):
+    addition = 4 if check('player', t[0], t[1]) else 1
+    if t in a_star:
+        if a_star[t] > a_star[min_cell] + addition:
+            a_star[t] = a_star[min_cell] + addition
+            a_star_back[t] = min_cell
+    elif not check('wall', t[0], t[1]):
+        a_star[t] = a_star[min_cell] + addition
+        a_star_back[t] = min_cell
+
+
+def find_path(check, start: (int, int), find: (int, int), iters: int) -> ((int, int), int):
+    a_star = {start: 0}
+    a_star_back = {}
+    a_star_calculated = set()
+    while True:
+        if iters != 0:
+            iters -= 1
+        else:
+            raise PathFindError("path is too long")
+        key_min = lambda xy: a_star[xy] + potential_len(xy, find) if not xy in a_star_calculated else 99999999
+        min_cell = min([*a_star], key=key_min)
+        a_star_calculated |= {min_cell}
+
+        check_and_append(check, a_star, a_star_back, (min_cell[0] - 1, min_cell[1]), min_cell)
+        check_and_append(check, a_star, a_star_back, (min_cell[0] + 1, min_cell[1]), min_cell)
+        check_and_append(check, a_star, a_star_back, (min_cell[0], min_cell[1] - 1), min_cell)
+        check_and_append(check, a_star, a_star_back, (min_cell[0], min_cell[1] + 1), min_cell)
+
+        if find in a_star:
+            break
+
+    back = find
+    while a_star_back[back] != start:
+        back = a_star_back[back]
+    return back, a_star[find]
+
+
+def script(check, x, y):
+    if check('gold', x, y):
+        return 'take'
+
+    gold = set()
+    depth = 1
+    while len(gold) == 0:
+        if depth > 100:
+            return random_work()
+        gold |= find_gold(check, x, y, depth)
+        depth += 1
+    for _ in range(4):
+        gold |= find_gold(check, x, y, depth)
+        depth += 1
+
+    gold = list(gold)
+    gold.sort(key=lambda xy: potential_len((x, y), xy))
+
+    a_star_gold = []
+
+    for g in gold:
+        try:
+            a_star_gold.append(find_path(check, (x, y), g, 1000))
+        except PathFindError:
+            pass
+
+    if len(a_star_gold) == 0:
+        return random_work()
+
+    a_star_gold.sort(key=lambda xy_l: xy_l[1] - gold_value(check, xy_l[0][0], xy_l[0][1]))
+
+    if x - a_star_gold[0][0][0] < 0:
+        return 'right'
+    if x - a_star_gold[0][0][0] > 0:
+        return 'left'
+    if y - a_star_gold[0][0][1] < 0:
+        return 'down'
+    if y - a_star_gold[0][0][1] > 0:
+        return 'up'
