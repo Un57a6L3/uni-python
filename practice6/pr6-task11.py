dfd882c3319092af47bf4622f66364a6580982bd
Arseny Antonov <un57a6l3@gmail.com>
2022-08-23 12:38:01 +0300

    Code cleanup: pr6t11

--- practice6/pr6-task11.py
+++ practice6/pr6-task11.py
@@ -3,66 +3,80 @@ Module to plot the Julia set (formula: `f(z) = z**2 + c`,
 where `z` is a complex variable and `c` is a complex constant).
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-
+import sys
 from math import sqrt
 from random import random
+
+import matplotlib.pyplot as plt
 from matplotlib import colors
-
-# Plot size in pixels
-N, M = 1000, 1000
-
-# Real and imaginary parts of C
-CX, CY = -0.74543, 0.11301
-# CX, CY = -0.75, 0.11
-# CX, CY = -0.1, 0.651
-
-# Radius: must be such that r**2 - r >= sqrt(cx**2 + cy**2)
-radius = 0
-while radius ** 2 - radius < sqrt(CX ** 2 + CY ** 2):
-    radius += 0.1
+from numpy import linspace, vstack
 
 
-def initplane(n, m, cx, cy, r):
+def initplane(n, m, cx, cy, r, max_iter, prog=True):
     """Function to calculate the plane."""
 
     # Initializing plane
     plane = [[None] * m for _ in range(n)]
 
+    # Outer loop (rows): Print progress
     for i in range(n):
+
+        # Printing calculation progress
+        if prog:
+            sys.stdout.write(f"\rCalculating row {i + 1}/{n}")
+            sys.stdout.flush()
+
+        # Inner loop (cols): Calculate pixel value
         for j in range(m):
-            # scaling to be between -R and R
+
+            # Scaling to be between -R and R
             zx = -r + i * (2 * r / n)
             zy = -r + j * (2 * r / m)
 
-            iteration = 0
-            max_iteration = 100
-
-            # calculating next value
-            while zx ** 2 + zy ** 2 < r ** 2 and iteration < max_iteration:
+            # Calculating next value
+            iter = 0
+            while zx ** 2 + zy ** 2 < r ** 2 and iter < max_iter:
                 xtemp = zx ** 2 - zy ** 2
                 zy = 2 * zx * zy + cy
                 zx = xtemp + cx
-                iteration += 1
+                iter += 1
+            plane[i][j] = iter
 
-            plane[i][j] = iteration
     return plane
 
 
-def add_white(colormap):
-    # doesn't conflict with vstack
-    black = plt.cm.binary(255)
-    # 255 because 1 is taken by black
-    colormap = colormap(np.linspace(0, 1, 255))
-    # stack both together
-    colormap = np.vstack((colormap, black))
-    # cast to MPL colormap type
+def add_black(colormap):
+    """Alters a colormap so that the highest value is black."""
+
+    black = plt.cm.binary(255)                # doesn't conflict with vstack
+    colormap = colormap(linspace(0, 1, 255))  # 255 for colormap, 1 for black
+    colormap = vstack((colormap, black))      # stack both together
+
+    # Сast to MPL colormap type
     return colors.LinearSegmentedColormap.from_list('my_colormap', colormap)
 
 
+def main():
+    # Input values
+    n, m = list(map(int, input("Enter resolution (N, M): ").split()))
+    cx, cy = list(map(float, input("Enter C constant (cx, cy): ").split()))
+    max_iter = int(input("Enter max iterations per pixel (default 100): "))
+
+    # Radius must be such that r**2 - r >= sqrt(cx**2 + cy**2)
+    radius = 0
+    while radius ** 2 - radius < sqrt(cx ** 2 + cy ** 2):
+        radius += 0.1
+
+    # Calculate plane
+    plane = initplane(n, m, cx, cy, radius, max_iter)
+
+    # Plot plane and save image
+    cmap = add_black(plt.cm.viridis)
+    norm = plt.Normalize(vmin=0, vmax=max_iter)
+    image = cmap(norm(plane))
+    name = f"julia_normal_{n}x{m}_c={cx}{'+' if cy > 0 else '-'}{abs(cy)}i"
+    plt.imsave(f"{name}.png", image)
+
+
 if __name__ == "__main__":
-    plane = initplane(N, M, CX, CY, radius)
-    cmap = add_white(plt.cm.viridis)
-    plt.imsave(f"julia_normal_{N}x{M}_c={CX}{'+' if CY>0 else '-'}{abs(CY)}i.png",
-           plane, cmap=cmap)
+    main()
